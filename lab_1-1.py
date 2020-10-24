575b6ecaf5c7377471b76d71feddf0bf45f89f08
christian lalanne <p21clalanne@fairfieldprep.org>
2020-10-24 13:05:03 -0400

    lab 1-1 done

---
+++ lab_1-1.py
@@ -0,0 +1,16 @@
+# Author CL 10/23/2020
+
+# int(a)
+# Name Error 
+
+# int('a')
+# ValueError
+
+# 'a' + 2
+# TypeError 
+
+# important date
+# ModuleNotFoundError
+
+# print("I'm a happy camper!")
+# SyntaxError
