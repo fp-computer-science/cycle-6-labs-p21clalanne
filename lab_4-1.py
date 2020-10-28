626ebb23139f77e2cd2c45696648393c7ae68e77
Christian Lalanne <p21clalanne@fairfieldprep.org>
2020-10-28 13:10:55 -0400

    lab 4-1 done

---
+++ lab_4-1.py
@@ -0,0 +1,26 @@
+# Author CL 10/28/2020
+
+import time
+import math
+
+# math.pow function
+t0 = time.perf_counter()
+
+math.pow(2, 2)
+
+t1 = time.perf_counter()
+
+speed1 = t1 - t0
+print(speed1)
+
+# ** operator
+t2 = time.perf_counter()
+
+2 ** 2
+
+t3 = time.perf_counter()
+
+speed2 = t3 - t2
+print(speed1)
+
+print(speed1 - speed2)
