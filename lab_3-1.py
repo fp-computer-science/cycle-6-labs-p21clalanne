2ae35e941d1674fbe46786a5d1c4858c3b8edf00
Christian Lalanne <p21clalanne@fairfieldprep.org>
2020-10-28 13:20:06 -0400

    lab 3-1 done

---
+++ lab_3-1.py
@@ -0,0 +1,14 @@
+# Author CL 10/27/2020
+
+import math
+
+print(math.ceil(7.15))
+print(math.floor(19.156))
+print(math.fabs(-42))
+print(math.pow(2, 3))
+print(math.log10(100))
+print(math.sqrt(9))
+print(math.isqrt(2))
+print(math.factorial(5))
+print(math.gcd(111111111, 12321))
+print(math.sin(0))
